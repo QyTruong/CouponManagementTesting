a2ab97279476c7c0079ee43e01033c7857d98431
le-thanh-truong <lettruong123@gmail.com>
2026-04-05 13:30:40 +0700

    change password database

--- app/__init__.py
+++ app/__init__.py
@@ -18,7 +18,7 @@ def create_app(test_config=None):
     if test_config:
         app.config.update(test_config)
     else:
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123456@localhost/coupondb?charset=utf8mb4'
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/coupondb?charset=utf8mb4'
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
     app.config['PAGE_SIZE'] = 6
